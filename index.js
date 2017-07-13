8b6c873a517a91f3358996be0a71b36b7c0d274c
Anshul Prakash <anshul.prakash17@gmail.com>
2017-07-13 10:17:02 -0500

    fetching all task lists and displaying on screen

--- index.js
+++ index.js
@@ -19,88 +19,101 @@ const fs = require('fs');
 const readline = require('readline');
 const google = require('googleapis');
 const googleAuth = require('google-auth-library');
+process.env.DEBUG = 'actions-on-google:*';
+const App = require('actions-on-google').ApiAiApp;
 
-
-exports.tasksWebhook = (req, res) => {
+exports.iamHome = (request, response) => {
+  const app = new App({ request, response });
+  console.log('Request headers: ' + JSON.stringify(request.headers));
+  console.log('Request body: ' + JSON.stringify(request.body));
   
-  authorize(listTaskLists);
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({ 'speech': "Done", 'displayText': "Done" }));
-};
-
-/**
- * Create an OAuth2 client with the given credentials, and then execute the
- * given callback function.
- *
- * @param {Object} credentials The authorization client credentials.
- * @param {function} callback The callback to call with the authorized client.
- */
-function authorize(callback) {
-  var clientSecret = 'e0cQ2c-f2NLCDK8TkpErzLRF';
-  var clientId = '982354824917-qql3onj149nhoe1c801e5kk0jos826er.apps.googleusercontent.com';
-  var redirectUrl = 'urn:ietf:wg:oauth:2.0:oob';
-  var auth = new googleAuth();
+  let clientSecret = 'e0cQ2c-f2NLCDK8TkpErzLRF';
+  let clientId = '982354824917-qql3onj149nhoe1c801e5kk0jos826er.apps.googleusercontent.com';
+  let redirectUrl = 'urn:ietf:wg:oauth:2.0:oob';
+  let auth = new googleAuth();
   var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
-  var token = '{"access_token":"ya29.GluFBEbuA01kY9Eo3fF6RTvAXr4JaCs2aWHxZ3-KQejJWE7b_-2-OxPpExJMzHhZ3YdCrya3gJrlqyb2JKGMM4xr-Gto-aBXuh20syqECWas9nghnLTr3nmeclbW",'+
+  let token = '{"access_token":"ya29.GluFBEbuA01kY9Eo3fF6RTvAXr4JaCs2aWHxZ3-KQejJWE7b_-2-OxPpExJMzHhZ3YdCrya3gJrlqyb2JKGMM4xr-Gto-aBXuh20syqECWas9nghnLTr3nmeclbW",'+
               '"refresh_token":"1/UUR_ORDDadCoQxdgZpfoACFeRYxtUY5lK-MK-ze6z_8",'+
               '"token_type":"Bearer","expiry_date":1499876043952}'
   oauth2Client.credentials = JSON.parse(token);
-  callback(oauth2Client);
-}
 
-/**
+  /**
  * Lists the user's first 10 task lists.
  *
  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
  */
-function listTaskLists(auth) {
-  var service = google.tasks('v1');
+function listTaskLists(app) {
+  let service = google.tasks('v1');
   service.tasklists.list({
-    auth: auth,
+    auth: oauth2Client,
     maxResults: 10,
-  }, function(err, response) {
+  }, function(err, res) {
     if (err) {
       console.log('The API returned an error: ' + err);
       return;
     }
-    var items = response.items;
+    let items = res.items;
     if (items.length == 0) {
       console.log('No task lists found.');
+      app.tell('No task lists found.');
+      return ;
     } else {
-      console.log('Task lists:');
-      for (var i = 0; i < items.length; i++) {
-        var item = items[i];
+      console.log('Task lists: ');
+      let response = 'Task lists: ';
+      for (let i = 0; i < items.length; i++) {
+        let item = items[i];
         console.log('%s (%s)', item.title, item.id);
-        listTaskListTasks(item.title,auth);
+        response += item.title + ' ';
       }
+      app.ask(app.buildRichResponse()
+            .addSimpleResponse(response));
+      return ;
     }
   });
 }
 
 /**
- * Lists the user's first 10 task lists.
+ * Lists tasks for a task list
  *
  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
  */
-function listTaskListTasks(tasklist,auth) {
-  var service = google.tasks('v1');
+function listTaskListTasks(app) {
+  let service = google.tasks('v1');
   service.tasks.list({
-    auth: auth,
+    auth: oauth2Client,
     tasklist: tasklist,
   }, function(err, response) {
     if (err) {
       console.log('The API returned an error: ' + err);
       return;
     }
-    var items = response.items;
+    let items = response.items;
     if (items.length == 0) {
       console.log('No task lists found.');
     } else {
       console.log('Tasks: ');
-      for (var i = 0; i < items.length; i++) {
-        var item = items[i];
+      for (let i = 0; i < items.length; i++) {
+        let item = items[i];
         console.log('%s (%s)', item.title, item.id);
       }
     }
   });
 }
+
+
+
+let actionMap = new Map();
+  actionMap.set('list.TaskLists', listTaskLists);
+
+  app.handleRequest(actionMap);
+
+};
+
+
+/* exports.tasksWebhook = (req, res) => {
+  
+  authorize(listTaskLists);
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({ 'speech': "Done", 'displayText': "Done" }));
+}; */
+

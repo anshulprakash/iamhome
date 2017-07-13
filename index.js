ac3f7dff195f0e8782d01a7f7303fbbcab45f046
Anshul Prakash <anshul.prakash17@gmail.com>
2017-07-13 08:34:06 -0500

    fetching task lists but not tasks

--- index.js
+++ index.js
@@ -13,60 +13,94 @@
 
 'use strict';
 const http = require('http');
-const host = 'api.worldweatheronline.com';
-const wwoApiKey = 'b7b9ffe1c05e42df8eb193803171007';
-exports.weatherWebhook = (req, res) => {
-  // Get the city and date from the request
-  let city = req.body.result.parameters['geo-city']; // city is a required param
-  let userName = req.body.result.parameters['given_name'];
-  console.log('User Name: '+userName)
-  // Get the date for the weather forecast (if present)
-  let date = '';
-  if (req.body.result.parameters['date']) {
-    date = req.body.result.parameters['date'];
-    console.log('Date: ' + date);
-  }
-  // Call the weather API
-  callWeatherApi(city, date).then((output) => {
-    // Return the results of the weather API to API.AI
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({ 'speech': output, 'displayText': output }));
-  }).catch((error) => {
-    // If there is an error let the user know
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({ 'speech': error, 'displayText': error }));
-  });
+//const host = 'api.worldweatheronline.com';
+//const wwoApiKey = 'b7b9ffe1c05e42df8eb193803171007';
+const fs = require('fs');
+const readline = require('readline');
+const google = require('googleapis');
+const googleAuth = require('google-auth-library');
+
+
+exports.tasksWebhook = (req, res) => {
+  
+  authorize(listTaskLists);
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({ 'speech': "Done", 'displayText': "Done" }));
 };
-function callWeatherApi (city, date) {
-  return new Promise((resolve, reject) => {
-    // Create the path for the HTTP request to get the weather
-    let path = '/premium/v1/weather.ashx?format=json&num_of_days=1' +
-      '&q=' + encodeURIComponent(city) + '&key=' + wwoApiKey + '&date=' + date;
-    console.log('API Request: ' + host + path);
-    // Make the HTTP request to get the weather
-    http.get({host: host, path: path}, (res) => {
-      let body = ''; // var to store the response chunks
-      res.on('data', (d) => { body += d; }); // store each response chunk
-      res.on('end', () => {
-        // After all the data has been received parse the JSON for desired data
-        let response = JSON.parse(body);
-        let forecast = response['data']['weather'][0];
-        let location = response['data']['request'][0];
-        let conditions = response['data']['current_condition'][0];
-        let currentConditions = conditions['weatherDesc'][0]['value'];
-        // Create response
-        let output = `Current conditions in the ${location['type']} 
-        ${location['query']} are ${currentConditions} with a projected high of
-        ${forecast['maxtempC']}°C or ${forecast['maxtempF']}°F and a low of 
-        ${forecast['mintempC']}°C or ${forecast['mintempF']}°F on 
-        ${forecast['date']}.`;
-        // Resolve the promise with the output text
-        console.log(output);
-        resolve(output);
-      });
-      res.on('error', (error) => {
-        reject(error);
-      });
-    });
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ *
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(callback) {
+  var clientSecret = 'e0cQ2c-f2NLCDK8TkpErzLRF';
+  var clientId = '982354824917-qql3onj149nhoe1c801e5kk0jos826er.apps.googleusercontent.com';
+  var redirectUrl = 'urn:ietf:wg:oauth:2.0:oob';
+  var auth = new googleAuth();
+  var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+  var token = '{"access_token":"ya29.GluFBEbuA01kY9Eo3fF6RTvAXr4JaCs2aWHxZ3-KQejJWE7b_-2-OxPpExJMzHhZ3YdCrya3gJrlqyb2JKGMM4xr-Gto-aBXuh20syqECWas9nghnLTr3nmeclbW",'+
+              '"refresh_token":"1/UUR_ORDDadCoQxdgZpfoACFeRYxtUY5lK-MK-ze6z_8",'+
+              '"token_type":"Bearer","expiry_date":1499876043952}'
+  oauth2Client.credentials = JSON.parse(token);
+  callback(oauth2Client);
+}
+
+/**
+ * Lists the user's first 10 task lists.
+ *
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+function listTaskLists(auth) {
+  var service = google.tasks('v1');
+  service.tasklists.list({
+    auth: auth,
+    maxResults: 10,
+  }, function(err, response) {
+    if (err) {
+      console.log('The API returned an error: ' + err);
+      return;
+    }
+    var items = response.items;
+    if (items.length == 0) {
+      console.log('No task lists found.');
+    } else {
+      console.log('Task lists:');
+      for (var i = 0; i < items.length; i++) {
+        var item = items[i];
+        console.log('%s (%s)', item.title, item.id);
+        listTaskListTasks(item.title,auth);
+      }
+    }
+  });
+}
+
+/**
+ * Lists the user's first 10 task lists.
+ *
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+function listTaskListTasks(tasklist,auth) {
+  var service = google.tasks('v1');
+  service.tasks.list({
+    auth: auth,
+    tasklist: tasklist,
+  }, function(err, response) {
+    if (err) {
+      console.log('The API returned an error: ' + err);
+      return;
+    }
+    var items = response.items;
+    if (items.length == 0) {
+      console.log('No task lists found.');
+    } else {
+      console.log('Tasks: ');
+      for (var i = 0; i < items.length; i++) {
+        var item = items[i];
+        console.log('%s (%s)', item.title, item.id);
+      }
+    }
   });
 }
